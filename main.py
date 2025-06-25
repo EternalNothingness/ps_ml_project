f38d5ab0cbb47baf8643531636a2d7f0e8534faf
Patrick Wintner <p.wintner@aon.at>
2025-06-25 19:13:27 +0200

    changed step size back

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ X_train, X_test, y_train, y_test = rf.load_and_split()
 
 # find hyperparameters and train the random forest
 n_features = X_train.shape[1]
-step_size = int(n_features/4)
+step_size = int(n_features/5)
 clf_params = {rf.PIPE_PCA + '__n_components': [i for i in range(1, n_features, step_size)], rf.PIPE_RF + '__n_estimators': np.random.randint(10, 100, 100), rf.PIPE_RF + '__max_features': np.random.rand(100), rf.PIPE_RF + '__criterion': ['gini', 'entropy', 'log_loss']}
 clf = rf.train(X_train, y_train, clf_params)
 
