6e39075a9b4a7d8a232989aeb3d347329c37f076
Patrick Wintner <p.wintner@aon.at>
2025-06-25 19:06:11 +0200

    finished coding

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import pandas as pd
+import rf
 import numpy as np
-import os
-from sklearn.metrics import roc_auc_score
-from sklearn.ensemble import RandomForestClassifier
-import pathlib
+import joblib as jl
 
-path = "/data/mlproject22" if os.path.exists("/data/mlproject22") else "."
-train_data = pd.read_csv(os.path.join(path, "transactions.csv.zip"))
-X_train = train_data.drop(columns = "Class")
-y_train = train_data["Class"]
+# load data and split it into train and test set
+X_train, X_test, y_train, y_test = rf.load_and_split()
+
+# find hyperparameters and train the random forest
+n_features = X_train.shape[1]
+step_size = int(n_features/5)
+clf_params = {rf.PIPE_PCA + '__n_components': [i for i in range(1, n_features, step_size)], rf.PIPE_RF + '__n_estimators': np.random.randint(10, 100, 100), rf.PIPE_RF + '__max_features': np.random.rand(100), rf.PIPE_RF + '__criterion': ['gini', 'entropy', 'log_loss']}
+clf = rf.train(X_train, y_train, clf_params)
+
+# store the model
+jl.dump(clf, "pca_rf.jl")
+
+# evaluate the random forest and print the results
+rf.evaluate(clf, X_test, y_test)
---
+++ rf.py
@@ -0,0 +1,34 @@
+
+import pandas as pd
+import os
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
+import sklearn.metrics
+
+PIPE_PCA = 'pca'
+PIPE_RF = 'rf'
+
+def load_and_split():
+    path = "."
+    data = pd.read_csv(os.path.join(path, "transactions.csv.zip"))
+    X = data.drop(columns = "Class")
+    y = data["Class"]
+    return train_test_split(X, y, random_state=0)
+
+def train(X_train, y_train, clf_params):
+    # clf = RandomForestClassifier(random_state=0, verbose=1)
+    pca = PCA(random_state=0)
+    rf = RandomForestClassifier(random_state=0)
+    pipeline = Pipeline(steps=[(PIPE_PCA, pca), (PIPE_RF, rf)])
+    search = RandomizedSearchCV(estimator=pipeline, param_distributions=clf_params, scoring='roc_auc', refit=True, random_state=0, n_jobs=-1, verbose=2)
+    search.fit(X_train, y_train)
+    return search.best_estimator_
+
+def evaluate(clf, X_test, y_test):
+    print('roc_auc_score=%f' %(sklearn.metrics.roc_auc_score(y_test, clf.predict(X_test))))
+    print('accuracy_score=%f' %(sklearn.metrics.accuracy_score(y_test, clf.predict(X_test))))
+    print('precision_score=%f' %(sklearn.metrics.precision_score(y_test, clf.predict(X_test))))
+    print('recall_score=%f' %(sklearn.metrics.recall_score(y_test, clf.predict(X_test))))
