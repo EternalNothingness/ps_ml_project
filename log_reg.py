f79baf83bc3812694fbcbf230b2b4247efe12526
csbb3133 <Dominik.Gajdos@student.uibk.ac.at>
2025-07-06 16:01:41 +0200

    added Log Reg to the Report

--- log_reg.py
+++ log_reg.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, roc_auc_score, roc_curve, auc, precision_recall_curve
+from sklearn.metrics import accuracy_score
 import joblib
 
 import matplotlib.pyplot as plt
@@ -93,40 +94,17 @@ def plot_precision(precision, recall, thresholds):
     plt.legend()
     plt.show()
 
-def load_split_data(data, correlations, split_method, top_n_features, test_size):
-    """
-    Loads dataset, selects top correlated features with the target, splits into train/test,
-    and scales the features.
-    
-    Parameters:
-    - filepath: str, path to CSV dataset
-    - target_col: str, name of the target column
-    - top_n_features: int, number of top correlated features to use
-    - test_size: float, proportion of test data
-    - random_seed: int, random seed for reproducibility
-    
-    Returns:
-    - X_train_scaled, X_test_scaled: scaled feature arrays for train/test
-    - Y_train, Y_test: target arrays for train/test
-    - scaler: fitted StandardScaler object
-    - features: list of selected feature names
-    """
+def load_split_data(data, correlations, top_n_features, test_size):
     
     features = correlations.head(top_n_features).index.tolist()
     #print("\nTop correlated features with 'Class':\n", correlations.head(27))
 
     X = data[features].values           # select features,  columns 'FeatureXX' from the dataset
-    Y = data['Class'].values         # select target, column 'Class' from the dataset
+    Y = data['Class'].values            # select target, column 'Class' from the dataset
     
-    if (split_method == 1):
-        X_train, X_test, Y_train, Y_test = train_test_split(
-            X, Y, test_size=test_size, random_state=random_seed, stratify=Y # preserves fraud/non-fraud ratio
-        )
-    else:
-        sss = StratifiedShuffleSplit(n_splits=1, test_size=t_size, random_state=random_seed)
-        for train_idx, test_idx in sss.split(X, Y):
-            X_train, X_test = X[train_idx], X[test_idx]
-            Y_train, Y_test = Y[train_idx], Y[test_idx]
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=test_size, random_state=random_seed, stratify=Y # preserves fraud/non-fraud ratio
+    )
     
     # scale features
     scaler = StandardScaler()
@@ -144,80 +122,107 @@ def load_split_data(data, correlations, split_method, top_n_features, test_size)
 def compute_best_threshold(precision, recall, thresholds):
     f1_scores = 2 * (precision * recall) / (precision + recall)
     best_idx = f1_scores.argmax()
-    #print(f"Best threshold by F1: {thresholds[best_idx]:.3f}")
     return thresholds[best_idx]
 
 # load data
 transactions_dataset = pd.read_csv('transactions.csv')
-#print(transactions_dataset.head())
-#print(transactions_dataset["Class"].value_counts())
 
 # compute correlations with target
 correlations = transactions_dataset.corr()['Class'].drop('Class').abs().sort_values(ascending=False)
 
-feature_range = range(3, 27, 1)
-test_sizes = np.arange(0.2, 0.5, 0.1)
+feature_range = range(3, 27, 1) 
+test_sizes = np.arange(0.2, 0.3, 0.1)
 best_config = {"f1": 0, "features": 0, "test_size": 0, "threshold": 0}
 best_roc_auc = {"score": 0, "features": 0, "test_size": 0}
+best_model = {"test_roc_auc": 0, "train_roc_auc": 0, "features": 0, "test_size": 0, "threshold": 0, "f1": 0, "precision": 0, "recall": 0, "accuracy": 0}
 
 for t_size in test_sizes:
     for n_features in feature_range:
-        print(f"\n=== Top {n_features} Features | Test Size: {t_size:.1f} ===")
 
         X_train_scaled, X_test_scaled, Y_train, Y_test, scaler, features = load_split_data(
             transactions_dataset, 
-            correlations,
-            1,    
+            correlations, 
             n_features,
             t_size)
         
         model = LogisticRegression(class_weight='balanced')
         model.fit(X_train_scaled, Y_train)
-        y_pred = model.predict(X_test_scaled)
-        y_scores = model.decision_function(X_test_scaled)
-        roc_auc = roc_auc_score(Y_test, y_scores)
 
-        print("ROC-AUC Score:", roc_auc)
-        
-        report = classification_report(Y_test, y_pred, output_dict=True)
-        class_1_metrics = report['1']
-        print(f"Class 1 - Precision: {class_1_metrics['precision']:.2f}, "
-            f"Recall: {class_1_metrics['recall']:.2f}, "
-            f"F1-Score: {class_1_metrics['f1-score']:.2f}")
-        
+        train_scores = model.decision_function(X_train_scaled)
+        train_roc_auc = roc_auc_score(Y_train, train_scores)
+
+        y_pred = model.predict(X_test_scaled)
+        acc = accuracy_score(Y_test, y_pred)
+
+        y_scores = model.decision_function(X_test_scaled)   # value before applying sigmoid function
+        roc_auc = roc_auc_score(Y_test, y_scores)       # does not change with thresholding
+
+        # change threshold to optimize F1 score
         precision, recall, thresholds = precision_recall_curve(Y_test, y_scores)
         best_threshold = compute_best_threshold(precision, recall, thresholds)
         y_pred_threshold = (y_scores >= best_threshold).astype(int)
         
         reportThreshold = classification_report(Y_test, y_pred_threshold, output_dict=True)
         class_2_metrics = reportThreshold['1']
-        print(f"Class 1 (Best Threshold) - Precision: {class_2_metrics['precision']:.2f}, "
-            f"Recall: {class_2_metrics['recall']:.2f}, "
-            f"F1-Score: {class_2_metrics['f1-score']:.2f}")
 
+        # update best configurations based on F1 and ROC-AUC
         if class_2_metrics['f1-score'] > best_config["f1"]:
             best_config.update({
                 "f1": class_2_metrics['f1-score'],
                 "features": n_features,
                 "test_size": t_size,
-                "threshold": best_threshold
+                "threshold": best_threshold,
+                "train_roc_auc": train_roc_auc,
+                "test_roc_auc": roc_auc
             })
 
         if roc_auc > best_roc_auc["score"]:
             best_roc_auc.update({
                 "score": roc_auc,
                 "features": n_features,
-                "test_size": t_size
+                "test_size": t_size,
+                "f1": class_2_metrics['f1-score'],
+                "test_roc_auc": roc_auc
             })
+
+        if train_roc_auc > 0.8 and roc_auc > best_model["test_roc_auc"]:
+            best_model.update({
+                "test_roc_auc": roc_auc,
+                "train_roc_auc": train_roc_auc,
+                "features": n_features,
+                "test_size": t_size,
+                "threshold": best_threshold,
+                "accuracy": acc,
+                "f1": class_2_metrics['f1-score'],
+                "precision": class_2_metrics['precision'],
+                "recall": class_2_metrics['recall']
+        })
         
 print("\n=== Best F1 Configuration ===")
 print(f"Top {best_config['features']} features | Test size: {best_config['test_size']}")
 print(f"Best F1: {best_config['f1']:.2f} at threshold: {best_config['threshold']:.4f}")
-print(f"Best ROC-AUC: {roc_auc_score(Y_test, y_scores):.3f}")
+print(f"ROC-AUC: {roc_auc_score(Y_test, y_scores):.4f}")
+print(f"Train ROC-AUC: {best_config['train_roc_auc']:.4f}")
 
 print("\n=== Best ROC-AUC Configuration ===")
 print(f"Top {best_roc_auc['features']} features | Test size: {best_roc_auc['test_size']}")
 print(f"Best ROC-AUC Score: {best_roc_auc['score']:.4f}")
+print(f"F1 Score: {best_roc_auc['f1']:.4f}")
+
+print("\n=== Best Model Based on Balanced ROC-AUC ===")
+print(f"Number of Features: {best_model['features']}")
+print(f"Test Set Size: {best_model['test_size']:.2f}")
+print(f"Best Threshold: {best_model['threshold']:.4f}")
+print(f"Train ROC-AUC: {best_model['train_roc_auc']:.4f}")
+print(f"Test ROC-AUC: {best_model['test_roc_auc']:.4f}")
+print(f"ROC-AUC Difference (Train - Test): {best_model['train_roc_auc'] - best_model['test_roc_auc']:.4f}")
+print(f"Accuracy: {best_model['accuracy']:.4f}")
+print(f"F1 Score: {best_model['f1']:.4f}")
+print(f"Precision: {best_model['precision']:.4f}")
+print(f"Recall: {best_model['recall']:.4f}")
+
+#plot_feature_distributions(transactions_dataset, features[20:27], target='Class', bins=60, grid_shape=(3, 3), figsize=(14, 10))
+#plot_roc_curve(model, X_test_scaled, Y_test)
 
 joblib.dump(model, "model.joblib")
 joblib.dump(scaler, "scaler.joblib")
